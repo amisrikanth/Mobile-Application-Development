acf1fde2cb2d6276335fe3953cb604bfbc3c4522
amisrikanth <amisrikanth@gmail.com>
2022-02-06 21:51:14 -0500

    finished development of the new activity

--- app/src/main/java/edu/neu/madcourse/numad22sp_srikanthbanagadi/MainActivity2.java
+++ app/src/main/java/edu/neu/madcourse/numad22sp_srikanthbanagadi/MainActivity2.java
@@ -3,6 +3,10 @@ package edu.neu.madcourse.numad22sp_srikanthbanagadi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    public void onButtonClick(View view) {
+        Button button = (Button) view;
+        TextView txt = (TextView) findViewById(R.id.textView2);
+        txt.setText("Pressed: " + button.getText());
     }
 }
