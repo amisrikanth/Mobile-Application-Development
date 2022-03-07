2e072ba4fb0bd587d53d1fd1df080cbeacf46ca8
amisrikanth <amisrikanth@gmail.com>
2022-03-06 21:41:27 -0500

    Created Service

--- app/src/main/java/edu/neu/madcourse/numad22sp_srikanthbanagadi/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad22sp_srikanthbanagadi/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     public void onClick(View view) {
-        Intent intent = new Intent(this,AboutMe.class);
+        Intent intent = new Intent(this,Service.class);
         startActivity(intent);
     }
 
